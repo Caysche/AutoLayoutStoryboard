b917228ec8d57e7d2743a51ee492d5e7559889d6
Caysche <cay.cornelius@gmail.com>
2016-09-15 14:17:57 -0700

    Initial commit with finished AutoLayoutStoryboard assignment

---
+++ AutoLayoutStoryboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayoutStoryboard
+//
+//  Created by Cay Cornelius on 15/09/16.
+//  Copyright © 2016 Cornelius.Media. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
